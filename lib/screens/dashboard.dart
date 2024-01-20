a4086ed7477affd7a9174f6172907f787fab5127
Saurabh Kumar <saurabhkumakairi501@gmail.com>
2024-01-20 18:25:27 +0530

    Home Screen

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -6,8 +6,68 @@ class DashBoardScrren extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const Drawer(),
       appBar: AppBar(
-        leading: const Drawer(),
+        title: const Text(
+          "Home",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        actions: const <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Stack(
+              children: [
+                CircleAvatar(
+                  backgroundImage:
+                      AssetImage("assets/images/profile_image.jpg"),
+                ),
+                Positioned(
+                    right: 5, child: Badge(backgroundColor: Colors.green))
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Text(
+              "Hello Maqsud!",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 190,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: const Column(
+                    children: [
+                      Text(
+                        "Attendence",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
